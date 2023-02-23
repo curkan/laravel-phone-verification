29f5b31623842b375dac7f501056bab38afb5da5
Nikita Tsurkan <nikita.tsurkan@softgamings.com>
2023-02-23 17:59:51 +0300

    Service provider

--- routes/api.php
+++ routes/api.php
@@ -5,5 +5,8 @@ use Gogain\LaravelPhoneVerification\Http\Controllers\ItemsController;
 use Gogain\LaravelPhoneVerification\Http\Controllers\SmsController;
 use Gogain\LaravelPhoneVerification\Http\Controllers\UsersController;
 
-Route::post('sms-verification', [SmsController::class, 'send'])->name('sms-verification');
-Route::post('sms-verification/{code}/{number}', [SmsController::class, 'checkCode'])->name('sms-verification.check');
+    Route::prefix('api')->group(function () {
+
+        Route::post('sms-verification', [SmsController::class, 'send'])->name('sms-verification');
+        Route::post('sms-verification/{code}/{number}', [SmsController::class, 'checkCode'])->name('sms-verification.check');
+    }
