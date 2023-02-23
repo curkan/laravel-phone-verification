ff014fcd63ab6a29d0834b0386644125b90e5fef
Nikita Tsurkan <nikita.tsurkan@softgamings.com>
2023-02-23 18:13:37 +0300

    Service provider

--- src/Providers/LaravelExampleServiceProvider.php
+++ src/Providers/LaravelExampleServiceProvider.php
@@ -11,8 +11,6 @@ class LaravelExampleServiceProvider extends ServiceProvider
     public function boot()
     {
         if ($this->app->runningInConsole()) {
-            $this->loadMigrationsFrom(__DIR__ . '/../../database/migrations');
-
             $this->publishes([
                 __DIR__ . '/../../config/sms-verification.php' => config_path('sms-verification.php'),
             ]);
@@ -23,7 +21,7 @@ class LaravelExampleServiceProvider extends ServiceProvider
         }
 
         $this->configureRoutes();
-        // $this->loadRoutesFrom(__DIR__ . '/../../routes/api.php');
+        $this->registerMigrations();
     }
     /**
      * Configure the routes offered by the application.
@@ -38,5 +36,18 @@ class LaravelExampleServiceProvider extends ServiceProvider
                  $this->loadRoutesFrom(__DIR__.'/../../routes/api.php');
              });
     }
+
+    /**
+     * Register the package's migrations.
+     *
+     * @return void
+     */
+    private function registerMigrations(): void
+    {
+        if ($this->app->runningInConsole()) {
+            $this->loadMigrationsFrom(__DIR__.'/../../database/migrations');
+        }
+    }
+
 
 }
