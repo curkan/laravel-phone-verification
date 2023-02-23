cf30c55b9714082e864a16c8dfab38903b7d25f4
Nikita Tsurkan <nikita.tsurkan@softgamings.com>
2023-02-23 22:52:05 +0300

    Verified Phone Rule

---
+++ src/Rules/VerifiedPhone.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Gogain\LaravelPhoneVerification\Rules;
+
+use Illuminate\Contracts\Validation\Rule;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Validator;
+
+final class VerifiedPhone implements Rule
+{
+    /**
+     * @param mixed $attribute
+     * @param mixed $value
+     *
+     * @return bool
+     */
+    public function passes($attribute, $value): bool
+    {
+        if (empty($value)) {
+            return false;
+        }
+        
+        if (!Cache::get('verified'.$value)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * @return string
+     */
+    public function message(): string
+    {
+        return 'Phone not verified';
+    }
+}
+
